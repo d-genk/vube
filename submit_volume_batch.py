fc9bff0bfbf129be75884e2bd47e28059ba4e8e2
Daniel Genkins <daniel.n.genkins@vanderbilt.edu>
2026-09-16 12:16:56 -0500

    processed volume update

--- submit_volume_batch.py
+++ submit_volume_batch.py
@@ -26,9 +26,7 @@ Example:
     python submit_volume_batch.py         --source-bucket my-source-bucket         --key-prefix collections/periodicals         --profile vube-source         --email you@example.org         --count 25
 """
 
-# python submit_volume_batch.py --source-bucket vubp-image-mls-825428742173-us-east-1-an --key-prefix Daniel --profile vube --email daniel.genkins@gmail.com --count 30
-
-# python submit_volume_batch.py --source-bucket vubp-image-mls-825428742173-us-east-1-an --key-prefix BP2/ --profile vube --folders-file "batches_02.csv" --dedup-index dedup_index.json --count 5 --email daniel.genkins@gmail.com --ledger submitted_jobs_bp2.jsonl
+# python submit_volume_batch.py --source-bucket vubp-image-mls-825428742173-us-east-1-an --key-prefix BP2/ --profile vube --folders-file "batches_02.csv" --dedup-index dedup_index.json --count 25 --email daniel.genkins@gmail.com --ledger submitted_jobs_bp2.jsonl
 
 import sys
 import time
